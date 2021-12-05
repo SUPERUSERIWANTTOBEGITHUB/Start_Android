e130d78a272e2bd94db7273d02ecf19fbf0ebdad
AS <yvg68@yandex.ru>
2021-12-05 10:44:32 +0300

    create onClick Button center

--- app/src/main/java/yvg68/measurer/start_andoid/MainActivity.kt
+++ app/src/main/java/yvg68/measurer/start_andoid/MainActivity.kt
@@ -2,10 +2,17 @@ package yvg68.measurer.start_andoid
 //
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun pushMe(view:View){
+        val pushMe = Toast.makeText(this, "Hello a new User!", Toast.LENGTH_SHORT)
+        pushMe.show()
+
     }
 }
