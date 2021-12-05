9058e513a9caf02fb93553fc9e93b0777cf20eec
AS <yvg68@yandex.ru>
2021-12-05 12:20:41 +0300

    create counter and textlabel

--- app/src/main/java/yvg68/measurer/start_andoid/MainActivity.kt
+++ app/src/main/java/yvg68/measurer/start_andoid/MainActivity.kt
@@ -3,16 +3,32 @@ package yvg68.measurer.start_andoid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
+
 class MainActivity : AppCompatActivity() {
+    lateinit var counView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    var count:Int = 0   //Integer.parseInt(counView.toString())
+    lateinit var quantity:TextView
+
+
     fun pushMe(view:View){
         val pushMe = Toast.makeText(this, "Hello a new User!", Toast.LENGTH_SHORT)
         pushMe.show()
+        counView = findViewById(R.id.countView)
+        count++
+        counView.text = count.toString()
+        quantity = findViewById(R.id.textLabel)
+        quantity.text ="QUANTITY:"
+
+
+
+
 
     }
 }
